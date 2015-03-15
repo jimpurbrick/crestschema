3f96e11bb25ad38952cf395ea9dadf66b1048da3
Jim Purbrick <jimp@fb.com>
2015-03-15 19:30:57 +0000

    Write schema files.

--- crestschemaspider.js
+++ crestschemaspider.js
@@ -1,4 +1,4 @@
-(function(https, stringify, crestschemaparser) {
+(function(https, fs, stringify, crestschemaparser) {
 
 var data = "";
 
@@ -13,10 +13,19 @@ var request = https.request({hostname: 'crest.eveonline.com',
 	    });
 	response.on('end', function() {
 		process.stdout.write(data);
-		process.stdout.write(stringify(crestschemaparser.jsonSchemaFromCrestOptions(data), {space: 4}));
+		var jsonSchema = crestschemaparser.jsonSchemaFromCrestOptions(data);
+		for (name in jsonSchema) {
+		    if (jsonSchema.hasOwnProperty(name)) {
+			var fileName = name.replace('application/vnd.ccp.eve.','').replace('+', '.') + '.schema';
+			fs.writeFile(fileName, 
+				     stringify(jsonSchema[name], {space: 4}), function (err) {
+					 if (err) throw err;
+				     });
+		    }
+		}
 	    });
     });
 
 request.end();
 
-}(require('https'), require('json-stable-stringify'), require('./crestschema')));
+}(require('https'), require('fs'), require('json-stable-stringify'), require('./crestschema')));
