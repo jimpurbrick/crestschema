f2cf238c085160305f1fdd4b44c09694bd6b2e12
Jim Purbrick <jimp@fb.com>
2015-03-18 16:16:35 +0000

    Pass root uri and headers from command line. Fix lint warnings.

--- crestschemaspider.js
+++ crestschemaspider.js
@@ -1,97 +1,129 @@
-(function(request, fs, stringify, crestschemaparser) {
+/*jslint todo: true, vars: true, forin: true, plusplus: true, bitwise: true, eqeq: true, maxerr: 50, indent: 4, node: true */
+
+(function (request, fs, stringify, extend, crestschemaparser) {
+
+    'use strict';
 
     var uriRegex = /^https?:\/\/\S+$/;
-    var uris = [];
-    var representations = {};
-    var requested = {};
+    var foundUris = [];
+    var requestedUris = {};
+    var foundRepresentations = {};
+    var requestedRepresentations = {};
 
     function findUris(schema, instance) {
-	var result = [];
-	if (!schema || !instance) {
-	    return result; // TODO: error...
-	}
-	switch(schema.type) {
-	case 'object':
-	    for (property in schema.properties) {
-		result = result.concat(findUris(schema.properties[property], instance[property]));
-	    }
-	    break;
-	case 'array':
-	    // Assume array items are homogenous, so only add uris found in first instance.
-	    result = result.concat(findUris(schema.items, instance[0]));
-	    break;
-	case 'string':
-	    if (uriRegex.test(instance) && !requested.hasOwnProperty(instance)) {
-		console.log('Found uri ' + stringify(instance));
-		result.push(instance);
-	    }
-	    break;
-	}
-	return result;
+        var result = [];
+        var property;
+        switch (schema.type) {
+        case 'object':
+            for (property in schema.properties) {
+                result = result.concat(
+                    findUris(schema.properties[property], instance[property])
+                );
+            }
+            break;
+        case 'array':
+            // Assume array items are homogenous,
+            // so only add uris found in first instance.
+            result = result.concat(findUris(schema.items, instance[0]));
+            break;
+        case 'string':
+            if (uriRegex.test(instance)
+                    && !requestedUris.hasOwnProperty(instance)) {
+                console.log('Found uri ' + stringify(instance));
+                result.push(instance);
+            }
+            break;
+        }
+        return result;
     }
 
-    function nextUri() {
-	if (uris.length > 0) {
-	    getRepresentations(uris.pop());
-	}
+    function nextUri(headers) {
+        if (foundUris.length > 0) {
+            getRepresentations(foundUris.pop(), headers);
+        }
     }
 
-    function getRepresentations(uri) {
-	console.log("Requesting " + stringify(uri));
-	requested[uri] = true;
-	request({ method: 'OPTIONS',
-		    uri: uri,
-		    headers: {
-		    'Accept': 'application/vnd.ccp.eve.Options-v1+json'
-			}
-	    },
-	    function (error, response, body) {
+    function getRepresentations(uri, headers) {
+        console.log("Requesting " + stringify(uri));
+        requestedUris[uri] = true;
+        request(
+            {
+                method: 'OPTIONS',
+                uri: uri,
+                headers: extend(
+                    {'Accept': 'application/vnd.ccp.eve.Options-v1+json'},
+                    headers
+                )
+            },
+            function (error, response, body) {
+                var name;
+                var jsonSchema;
 
-		// TODO: remove this when CREST content negotiation works and we get 406 errors.
-		if (!response || ! response.headers['content-type'] == 'application/vnd.ccp.eve.Options-v1+json') {
-		    nextUri();
-		    return;
-		}
+                if (error || !response) {
+                    nextUri(headers);
+                    return;
+                }
 
-		var name;
-		var jsonSchema;
-		try {
-		    jsonSchema = crestschemaparser.jsonSchemaFromCrestOptions(body);
-		} catch(err) {
-		    nextUri();
-		    return;
-		}
+                try {
+                    jsonSchema =
+                        crestschemaparser.jsonSchemaFromCrestOptions(body);
+                } catch (err) {
+                    nextUri(headers);
+                    return;
+                }
 
-		if (! jsonSchema) {
-		    nextUri();
-		    return;
-		}
+                for (name in jsonSchema) {
+                    if (jsonSchema.hasOwnProperty(name)
+                            && (!foundRepresentations.hasOwnProperty(name))) {
+			foundRepresentations[name] = true;
+                        console.log("Found representation " + stringify(name));
+                        fs.writeFile(
+                            name.replace('application/vnd.ccp.eve.', '')
+                                .replace('+', '.'),
+                            stringify(jsonSchema[name], {space: 4})
+                        );
+                    }
+                }
 
-		for (name in jsonSchema) {
-		    if (jsonSchema.hasOwnProperty(name) &&
-			(!representations.hasOwnProperty(name))) {
-			console.log("Found representation " + stringify(name));
-			var fileName = name.replace('application/vnd.ccp.eve.','').replace('+', '.');
-			fs.writeFile(fileName, 
-				     stringify(jsonSchema[name], {space: 4}), function (err) {
-					 if (err) throw err;
-				     });
-		    }
-		}
-
-		// TODO: request each representation when CREST content negotiation is fixed.
-		request(uri,
-			function (error, response, body) {
-			    name = response.headers['content-type'].split(';')[0];
-			    if(!representations.hasOwnProperty(name) && jsonSchema.hasOwnProperty(name)) {
-				representations[name] = true;
-				uris = uris.concat(findUris(jsonSchema[name], JSON.parse(body)));
-			    }
-			    nextUri();
-			});
-		nextUri();
-	    });
+                // TODO: request each representation when CREST
+                // content negotiation is fixed.
+                request(
+                    {uri: uri, headers: headers},
+                    function (error, response, body) {
+                        if (error) {
+                            nextUri(headers);
+                            return;
+                        }
+                        name = response.headers['content-type'].split(';')[0];
+                        if (!requestedRepresentations.hasOwnProperty(name)
+                                && jsonSchema.hasOwnProperty(name)) {
+                            requestedRepresentations[name] = true;
+                            foundUris = foundUris.concat(
+                                findUris(jsonSchema[name], JSON.parse(body))
+                            );
+                        }
+                        nextUri(headers);
+                    }
+                );
+                nextUri(headers);
+            }
+        );
     }
-    getRepresentations('https://crest.eveonline.com');
 
-}(require('request'), require('fs'), require('json-stable-stringify'), require('./crestschema')));
+    var index = 2; // Usage: node crestschemaspider.js uri "Header: value"...
+    var rootUri = process.argv[index];
+    var header;
+    var headers = {};
+    for (index = 3; index < process.argv.length; ++index) {
+        header = process.argv[index].split(':');
+        headers[header[0]] = header[1].trim();
+    }
+    getRepresentations(rootUri);
+
+}(
+    require('request'),
+    require('fs'),
+    require('json-stable-stringify'),
+    require('extend'),
+    require('./crestschema')
+));
